10799092c20ef0a5982c18aff1d66d5c841755f0
mor.aroesti <mor.aroesti@forcepoint.com>
2020-12-22 15:09:24 +0200

    - add '.gitIgnore' - refactor methods name - extract method - add 'Readme.txt' file

--- src/com/mor/everc/api/Petshop.java
+++ src/com/mor/everc/api/Petshop.java
@@ -9,53 +9,58 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 
-
 public class Petshop {
-public final String URI = "https://petstore.swagger.io/v2/";
+    public final String URI = "https://petstore.swagger.io/v2/";
 
-    public JsonObject createPet (Pet pet){
+
+    public JsonObject createPet(Pet pet) {
         System.out.println("We'll going to create your pet");
         Response response = postRestAction(convertPet(pet), "pet");  //Using extract method, transfer the 'endpoint' as a parameter
-        return new Gson().fromJson(response.getBody().asString(), JsonObject.class);  //
+        return getJsonObject(response.getBody().asString());  //
     }
-    public JsonObject getSinglePet(String id){
+
+    public JsonObject getSinglePet(String id) {
         System.out.println("Going to brings the pet: " + id + " from the server");
         Response response = getRestActionById("pet", id);
-        return new Gson().fromJson(response.getBody().asString(), JsonObject.class);
+        return getJsonObject(response.getBody().asString());
+    }
+
+    private JsonObject getJsonObject(String s) {
+        return new Gson().fromJson(s, JsonObject.class);
     }
 
 
-    public JsonObject convertPet(Pet pet){   //convert pet structure to JsonObject
+    public JsonObject convertPet(Pet pet) {   //convert pet structure to JsonObject
         String jsonBody = new GsonBuilder().create().toJson(pet);
-        return new Gson().fromJson(jsonBody, JsonObject.class);
+        return getJsonObject(jsonBody);
     }
 
 
     private Response postRestAction(JsonObject body, String endpoint) {
-        getRestAssuredConf(endpoint);
+        initRestAssured(endpoint);
 
         return RestAssured
                 .given()
                 .basePath(endpoint)
                 .contentType(ContentType.JSON)
-                .header("accept","application/json")
+                .header("accept", "application/json")
                 .body(body.toString())
                 .post();
     }
 
     //TODO: should adding a 'PUT' scenario to update utility
 
-    private Response getRestActionById(String endpoint,String id) {
-        getRestAssuredConf(endpoint);
+    private Response getRestActionById(String endpoint, String id) {
+        initRestAssured(endpoint);
         return RestAssured
                 .given()
                 .basePath(endpoint + "/" + id)
                 .contentType(ContentType.JSON)
-                .header("accept","application/json")
+                .header("accept", "application/json")
                 .get();
     }
 
-    private void getRestAssuredConf(String endpoint) {
+    private void initRestAssured(String endpoint) {
         RestAssured.baseURI = URI;
         RestAssured.basePath = endpoint;
         RestAssured.config = RestAssured.config().sslConfig(SSLConfig.sslConfig().allowAllHostnames());
